40cd640b3389133388f85a4f129f8025c198dee8
ihibisu <ihibisu@gmail.com>
2022-02-20 02:11:20 +0900

    🚀 Server Update

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = {
+  apps: [
+    {
+      name: "Smoosly",
+      script: "./server/index.js",
+      env: {
+        NODE_ENV: "production", // 배포환경시 적용될 설정 지정
+        PORT: "443", //443
+      },
+    },
+    {
+      name: "Smoosly_dev",
+      script: "./server/index.js",
+      watch: ["server/index.js", "server/router", "server/middleware"],
+      ingore_watch: ["server/log"],
+      env: {
+        NODE_ENV: "development",
+        PORT: "443", //443
+      },
+    },
+    {
+      name: "Smoosly_local",
+      script: "./server/index_local.js",
+      watch: ["server/index_local.js", "server/router", "server/middleware", "server/winston.js"],
+      ingore_watch: ["server/log"],
+      env: {
+        NODE_ENV: "development", // 배포환경시 적용될 설정 지정
+        PORT: "3000", //443
+      },
+    },
+  ],
+};
